77a03f575383911f40850685d484217a4f132013
He <he20001030@gmail.com>
2024-06-29 18:52:09 +0800

    test

---
+++ boot3-02-logging/src/main/java/com/atguigu/logging/Boot303LoggingApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//启动SpringBoot项目的主入口程序
+@SpringBootApplication   //这是一个SpringBoot应用
+public class Boot303LoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot303LoggingApplication.class, args);
+    }
+
+}
+
